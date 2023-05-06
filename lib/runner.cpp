6d7170ecf8346bdad152448d37fb8ff613ac287f
AlbouabidiMouadh <95055135+AlbouabidiMouadh@users.noreply.github.com>
2023-05-06 22:43:31 +0100

    fix game logic

--- lib/runner.cpp
+++ lib/runner.cpp
@@ -2,49 +2,73 @@
 
 Runner::Runner() {}
 
-void Runner::init(int size, int speed) {
+void Runner::init(int size, int speed)
+{
   _size = size;
   _x = 15 - _size / 2;
   _y = 35 - _size / 2;
   _velocity.x = speed;
   _velocity.y = speed;
   _score = 0;
+  bool _jump = false;
+  int _i = 0;
 }
 
-void Runner::draw(N5110 &lcd) {
+void Runner::draw(N5110 &lcd)
+{
   // draw the runner function we are using the rectqngle for the prototype
   lcd.drawRect(_x, _y, _size, _size, FILL_BLACK);
 }
 
-void Runner::update(UserInput input) {
+void Runner::update(UserInput input)
+{
   // updating the runner position
 
-  if (input.d == N) {
-    jump();
+  if (input.d == N and _jump == false) {_jump == true}
+  if(_jump == true)
+  {
+    if (_i < 5)
+    {
+      _i++;
+      _x += _i;
+    }
+    else if( _i < 10) {
+      _i++ ;
+      _x -= _i-5 ;
+    }
+    else {
+      _i = 0;
+      _jump = false
+    }
   }
+  
 
   // update score
 
   add_score();
 }
 
-void Runner::jump() {
-  for (int i = 0; i < 10; i++) {
-    if (i < 5)
-      _x = _x + 1;
-    else
-      _x = _x - 1;
-    thread_sleep_for(100);
-  }
-}
+// void Runner::jump()
+// {
+//   for (int i = 0; i < 10; i++)
+//   {
+//     if (i < 5)
+//       _x = _x + 1;
+//     else
+//       _x = _x - 1;
+//     thread_sleep_for(100);
+//   }
+// }
 
-void Runner::set_velocity(Position2D v) {
+void Runner::set_velocity(Position2D v)
+{
   printf("Ball: Velocity\n");
   _velocity.x = v.x;
   _velocity.y = v.y;
 }
 
-void Runner::set_pos(Position2D p) {
+void Runner::set_pos(Position2D p)
+{
   printf("Ball: Set Position\n");
   _x = p.x;
   _y = p.y;
