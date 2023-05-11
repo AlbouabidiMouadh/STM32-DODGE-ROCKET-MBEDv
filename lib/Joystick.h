96633a51a477ac8129a21a71d08a8131814e3496
AlbouabidiMouadh <95055135+AlbouabidiMouadh@users.noreply.github.com>
2023-05-11 12:35:55 +0100

    Update Joystick.h

--- lib/Joystick.h
+++ lib/Joystick.h
@@ -10,46 +10,6 @@
 
 
 
-/** Joystick Class
-@author Dr Craig A. Evans, University of Leeds
-@brief  Library for interfacing with analogue joystick
-
-Example:
-
-@code
-
-#include "mbed.h"
-#include "Joystick.h"
-
-//                  y     x     button
-Joystick joystick(PTB11,PTB10);
-
-int main() {
-    
-    joystick.init();
-    
-    while(1) {
-    
-        Vector2D coord = joystick.get_coord();
-        printf("Coord = %f,%f\n",coord.x,coord.y);
-        
-        Vector2D mapped_coord = joystick.get_mapped_coord(); 
-        printf("Mapped coord = %f,%f\n",mapped_coord.x,mapped_coord.y); 
-        
-        float mag = joystick.get_mag();
-        float angle = joystick.get_angle();
-        printf("Mag = %f Angle = %f\n",mag,angle);
-        
-        Direction d = joystick.get_direction();
-        printf("Direction = %i\n",d);
-    
-          
-        wait(0.5);
-    }
-    
-    
-}
-
 * @endcode
 */
 class Joystick
